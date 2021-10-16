a9ab5626d4299d15500ec262a6cd79eacd215fa7
Amine Haddad <ahaddad@e2r2p15.1337.ma>
2021-10-16 15:55:25 +0100

    just create the files

---
+++ Stack/Stack.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Stack.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/16 15:44:29 by ahaddad           #+#    #+#             */
+/*   Updated: 2021/10/16 15:45:28 by ahaddad          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifdef STACK_HPP
+#define STACK_HPP
+
+#include <iostream>
+
+class Stack
+{
+private:
+    /* data */
+public:
+    Stack(/* args */);
+    ~Stack();
+};
+
+Stack::Stack(/* args */)
+{
+}
+
+Stack::~Stack()
+{
+}
+
+
+#endif
---
+++ Vector/Vector.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahaddad <ahaddad@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/16 15:45:39 by ahaddad           #+#    #+#             */
+/*   Updated: 2021/10/16 15:46:05 by ahaddad          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+class Vector
+{
+private:
+    /* data */
+public:
+    Vector(/* args */);
+    ~Vector();
+};
+
+Vector::Vector(/* args */)
+{
+}
+
+Vector::~Vector()
+{
+}
+
+
+#endif
