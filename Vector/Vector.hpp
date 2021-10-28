d43cf8fd244bf3ce7a7dda4bdeb591dfa3d6dac5
ahaddad98 <amine.haddadd@gmail.com>
2021-10-28 18:43:47 +0100

    add prototype function in vector.hpp

--- Vector/Vector.hpp
+++ Vector/Vector.hpp
@@ -6,7 +6,7 @@
 /*   By: amine <amine@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 15:45:39 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/10/28 16:12:01 by amine            ###   ########.fr       */
+/*   Updated: 2021/10/28 18:42:36 by amine            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ namespace ft
     {
     public:
         typedef int size_type;
-
+        // create our iterator
         class iterator
         {
         public:
@@ -39,7 +39,9 @@ namespace ft
             typedef T *pointer;
             typedef std::forward_iterator_tag iterator_category;
             typedef int difference_type;
-            iterator(pointer ptr) : ptr_(ptr) {}
+            iterator(pointer ptr) : ptr_(ptr)
+            {
+            }
             self_type operator++()
             {
                 self_type i = *this;
@@ -51,15 +53,27 @@ namespace ft
                 ptr_++;
                 return *this;
             }
-            reference operator*() { return *ptr_; }
-            pointer operator->() { return ptr_; }
-            bool operator==(const self_type &rhs) { return ptr_ == rhs.ptr_; }
-            bool operator!=(const self_type &rhs) { return ptr_ != rhs.ptr_; }
+            reference operator*()
+            {
+                return *ptr_;
+            }
+            pointer operator->()
+            {
+                return ptr_;
+            }
+            bool operator==(const self_type &rhs)
+            {
+                return ptr_ == rhs.ptr_;
+            }
+            bool operator!=(const self_type &rhs)
+            {
+                return ptr_ != rhs.ptr_;
+            }
 
         private:
             pointer ptr_;
         };
-
+        // create our const iterator
         class const_iterator
         {
         public:
@@ -69,7 +83,9 @@ namespace ft
             typedef T *pointer;
             typedef int difference_type;
             typedef std::forward_iterator_tag iterator_category;
-            const_iterator(pointer ptr) : ptr_(ptr) {}
+            const_iterator(pointer ptr) : ptr_(ptr)
+            {
+            }
             self_type operator++()
             {
                 self_type i = *this;
@@ -81,107 +97,179 @@ namespace ft
                 ptr_++;
                 return *this;
             }
-            const reference operator*() { return *ptr_; }
-            const pointer operator->() { return ptr_; }
-            bool operator==(const self_type &rhs) { return ptr_ == rhs.ptr_; }
-            bool operator!=(const self_type &rhs) { return ptr_ != rhs.ptr_; }
+            const value_type &operator*()
+            {
+                return *ptr_;
+            }
+            const pointer operator->()
+            {
+                return ptr_;
+            }
+            bool operator==(const self_type &rhs)
+            {
+                return ptr_ == rhs.ptr_;
+            }
+            bool operator!=(const self_type &rhs)
+            {
+                return ptr_ != rhs.ptr_;
+            }
 
         private:
             pointer ptr_;
         };
-
+        // constructors
         vector(size_type size) : size_(size)
         {
-            data_ = new T[size_];
+            m_Data = new T[size_];
         }
 
-        size_type size() const { return size_; }
+        // Capacity:
+        size_type size() const
+        {
+            return size_;
+        }
+        size_type max_size() const
+        {
+            // pas encore
+            return 0;
+        }
+        // void resize (size_type n, value_type val = value_type())
+        // {
+        // }
+        //
+        size_type capacity() const
+        {
+            // pas encore
+            return 0;
+        }
+        bool empty() const
+        {
+            // pas encore
+        }
+        void reserve(size_type n)
+        {
+            // pas encore
+        }
 
+        // Element access:
         T &operator[](size_type index)
         {
             assert(index < size_);
-            return data_[index];
+            return m_Data[index];
         }
-
         const T &operator[](size_type index) const
         {
             assert(index < size_);
-            return data_[index];
+            return m_Data[index];
         }
+        // reference at (size_type n)
+        // {
+        //     // pas encore
+        // }
+        // const_reference at (size_type n) const
+        // {
+        //     // pas encore
+        // }
+        // reference front()
+        // {
+        //     // pas encore
+        // }
+        // const_reference front() const
+        // {
+        //     // pas encore
+        // }
+        // reference back()
+        // {
+        //     //  pas encore
+        // }
+        // const_reference back() const
+        // {
+        //     //  pas encore
+        // }
 
+        // Iterators:
         iterator begin()
         {
-            return iterator(data_);
+            return iterator(m_Data);
         }
 
         iterator end()
         {
-            return iterator(data_ + size_);
+            return iterator(m_Data + size_);
         }
 
         const_iterator begin() const
         {
-            return const_iterator(data_);
+            return const_iterator(m_Data);
         }
 
         const_iterator end() const
         {
-            return const_iterator(data_ + size_);
+            return const_iterator(m_Data + size_);
         }
+        //reverse_iterator rbegin()
+        // {
+        // }
+        //const_reverse_iterator rbegin() const
+        // {
+        // }
 
+        // Modifiers:
+        // template <class InputIterator>
+        // void assign (InputIterator first, InputIterator last)
+        // {
+        // }
+        // void assign (size_type n, const value_type& val)
+        // {
+        // }
+        // void push_back (const value_type& val)
+        // {
+        // }
+        // void pop_back()
+        // {
+        // }
+        // iterator insert (iterator position, const value_type& val);
+        // void insert (iterator position, size_type n, const value_type& val);
+        // template <class InputIterator>
+        // void insert (iterator position, InputIterator first, InputIterator last);
+        // iterator erase(iterator position);
+        // iterator erase(iterator first, iterator last);
+        // void swap (vector& x)
+        // void clear();
+
+        // allocators
+        // allocator_type get_allocator() const
+
+        // Non-member function overloads
+        // template <class T, class Alloc>
+        // bool operator==(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs);
+
+        // template <class T, class Alloc>
+        // bool operator!=(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs);
+
+        // template <class T, class Alloc>
+        // bool operator<(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs);
+
+        // template <class T, class Alloc>
+        // bool operator<=(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs);
+
+        // template <class T, class Alloc>
+        // bool operator>(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs);
+
+        // template <class T, class Alloc>
+        // bool operator>=(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs);
+
+        // template <class T, class Alloc>
+        // void swap(vector<T, Alloc> &x, vector<T, Alloc> &y)
+
+        // Template specializations
+        // template <class T, class Alloc = allocator<T>>
+        // class vector; // generic template
+        // template <class Alloc>
+        // class vector<bool, Alloc>; // bool specialization
     private:
-        T *data_;
+        T *m_Data;
         size_type size_;
     };
-    // template <typename T>
-    // class vector
-    // {
-    // public:
-    //     typedef const_iterator self_type;
-    //     typedef T value_type;
-    //     typedef T &reference;
-    //     typedef T *pointer;
-    //     typedef int difference_type;
-    //     typedef std::forward_iterator_tag iterator_category;
-    //     typedef int size_type;
-    //     // typedef iterator<value_type>                 iterator;
-    //     vector(size_type size) : size_(size)
-    //     {
-    //         data_ = new T[size_];
-    //     }
-
-    //     size_type size() const { return size_; }
-
-    //     T &operator[](size_type index)
-    //     {
-    //         assert(index < size_);
-    //         return data_[index];
-    //     }
-
-    //     const T &operator[](size_type index) const
-    //     {
-    //         assert(index < size_);
-    //         return data_[index];
-    //     }
-
-    // iterator begin()
-    // {
-    //     return iterator(data_);
-    // }
-
-    // iterator end()
-    // {
-    //     return iterator(data_ + size_);
-    // }
-
-    // const_iterator begin() const
-    // {
-    //     return const_iterator(data_);
-    // }
-
-    // const_iterator end() const
-    // {
-    //     return const_iterator(data_ + size_);
-    // }
 }
 #endif
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: amine <amine@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 15:46:32 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/10/28 16:12:11 by amine            ###   ########.fr       */
+/*   Updated: 2021/10/28 18:41:59 by amine            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,62 +16,23 @@
 #include <vector>
 #include "Vector/iterator.hpp"
 
-// template<typename T>
-
-// void printVector(const ft::Vector<T>& vect)
-// {
-//     int i = 0;
-//     // ft::vector_iterator<int> it;
-//     // std::vector<int> vect;
-//     // vect.PushBack(12);
-//     // vect.PushBack(122);
-//     // vect.PushBack(123);
-//     // vect.PushBack(124);
-//     // vect.PushBack(125);
-//     while (i <vect.Size())
-//     {
-//         std::cout << vect[i] << std::endl;
-//         i++;
-//     }
-// }
-
 int main()
 {
-    ft::vector<std::string> point3d(3);
-    point3d[0] = "2";
-    point3d[1] = "3";
-    point3d[2] = "4";
+    ft::vector<std::string> vect(3);
+    vect[0] = "2";
+    vect[1] = "3";
+    vect[2] = "4";
 
-    for (ft::vector<std::string>::iterator i = point3d.begin(); i != point3d.end(); i++)
+    for (ft::vector<std::string>::iterator i = vect.begin(); i != vect.end(); i++)
     {
         std::cout << *i << " ";
     }
-
     std::cout << std::endl;
-    // std::vector<int>::iterator it;
-    // it.__i = 13;
-    // ft::vector_iterator<int> iter;
-    // std::vector<int> vect;
-    // vect.push_back(12);
-    // vect.push_back(122);
-    // vect.push_back(123);
-    // vect.push_back(124);
-    // vect.push_back(125);
-    // // iter = vect.begin();
-    // std::cout << "amine" << std::endl;
-    // ft::Vector<int> vector;
-    // vector.PushBack(2);
-    // vector.PushBack(3);
-    // vector.PushBack(4);
-    //  vector.PopBack();
-    //  vector.clear();
-    // printVector(vector);
-    // std::vector<int> vect(10, 0);
-    // vect.assign("","");
-    // std::allocator<int> alloc;
-    // int *p = alloc.allocate(1);
-    // p[4] = 4;
-    // vect.
-    // alloc.deallocate();
     return 0;
 }
+
+// int main()
+// {
+
+//     return 0;
+// }
