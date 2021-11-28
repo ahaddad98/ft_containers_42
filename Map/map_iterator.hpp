4746342ecbaf120ffba86d0c6b653620bf9695f9
ahaddad98 <amine.haddadd@gmail.com>
2021-11-28 19:42:29 +0100

    add root next and root prev

--- Map/map_iterator.hpp
+++ Map/map_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: amine <amine@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 17:13:33 by ahaddad           #+#    #+#             */
-/*   Updated: 2021/11/28 14:36:09 by amine            ###   ########.fr       */
+/*   Updated: 2021/11/28 19:40:06 by amine            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,33 +57,72 @@ namespace ft
                 tmp = tmp->right;
             return tmp;
         }
-        t_Node *root_prev()
+        class iterator_map : public ft::Iterator_Traits<std::bidirectional_iterator_tag, T>
         {
-        }
-        t_Node *root_next()
-        {
-        }
-    };
-    template <typename T>
-    class iterator_map : public ft::Iterator_Traits<std::bidirectional_iterator_tag, T>
-    {
-    public:
-        typedef iterator_map self_type;
-        typedef typename ft::Iterator_Traits<std::bidirectional_iterator_tag, T>::value_type value_type;
-        typedef typename ft::Iterator_Traits<std::bidirectional_iterator_tag, T>::pointer pointer;
-        typedef typename ft::Iterator_Traits<std::bidirectional_iterator_tag, T>::reference reference;
-        typedef typename ft::Iterator_Traits<std::bidirectional_iterator_tag, T>::difference_type difference_type;
-        typedef typename ft::Iterator_Traits<std::bidirectional_iterator_tag, T>::iterator_categorie iterator_category;
+        public:
+            typedef iterator_map self_type;
+            typedef typename ft::Iterator_Traits<std::bidirectional_iterator_tag, T>::value_type value_type;
+            typedef typename ft::Iterator_Traits<std::bidirectional_iterator_tag, T>::pointer pointer;
+            typedef typename ft::Iterator_Traits<std::bidirectional_iterator_tag, T>::reference reference;
+            typedef typename ft::Iterator_Traits<std::bidirectional_iterator_tag, T>::difference_type difference_type;
+            typedef typename ft::Iterator_Traits<std::bidirectional_iterator_tag, T>::iterator_categorie iterator_category;
 
-        iterator_map(/* args */)
-        {
-        }
-        ~iterator_map()
-        {
-        }
+            iterator_map() : ptr_(NULL)
+            {
+            }
+            ~iterator_map()
+            {
+            }
+            t_Node *root_prev() const
+            {
+                const t_Node *tmp1 = this->ptr_;
+                if (ptr_->left)
+                {
+                    tmp1 = ptr_->left->max_of_tree();
+                }
+                else
+                {
+                    t_Node *tmp = this->ptr_->parent;
+                    while (tmp && tmp->left == tmp1)
+                    {
+                        tmp1 = tmp;
+                        tmp = tmp->parent;
+                    }
+                    if (tmp)
+                        tmp1 = tmp;
+                    else
+                        tmp1 = NULL;
+                }
+                return (tmp1);
+            }
 
-    private:
-        pointer ptr_;
+            t_Node *root_next() const
+            {
+                t_Node *next = this->ptr_;
+                if (this->ptr_->right != NULL)
+                {
+                    next = ptr_->right->min_of_tree();
+                }
+                else
+                {
+                    t_Node *tmp = this->ptr_->parent;
+                    while (tmp && tmp->right == next)
+                    {
+                        next = tmp;
+                        tmp = tmp->parent;
+                    }
+                    if (tmp)
+                        next = tmp;
+                    else
+                        next = NULL;
+                }
+                return (next);
+            }
+            
+        private:
+            t_Node *ptr_;
+            Red_Blacl_Tree *parent;
+        };
     };
 }
 #endif
